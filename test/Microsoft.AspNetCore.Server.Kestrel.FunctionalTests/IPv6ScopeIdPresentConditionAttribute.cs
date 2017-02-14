aae4dabe2fda3da75dfc75ad88fd5942546e5880
Cesar Blum Silveira <cesars@microsoft.com>
2017-02-14 10:53:06 -0800

    Fix AddressRegistrationTests.RegisterAddresses_IPv6ScopeId_Success (#1363).

---
+++ test/Microsoft.AspNetCore.Server.Kestrel.FunctionalTests/IPv6ScopeIdPresentConditionAttribute.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using Microsoft.AspNetCore.Testing.xunit;
+
+namespace Microsoft.AspNetCore.Server.Kestrel.FunctionalTests
+{
+    public class IPv6ScopeIdPresentConditionAttribute : Attribute, ITestCondition
+    {
+        private static readonly Lazy<bool> _ipv6Supported = new Lazy<bool>(CanBindToIPv6Address);
+
+        public bool IsMet
+        {
+            get
+            {
+                return _ipv6Supported.Value;
+            }
+        }
+
+        public string SkipReason
+        {
+            get
+            {
+                return "No IPv6 addresses with scope IDs were found on the host.";
+            }
+        }
+
+        private static bool CanBindToIPv6Address()
+        {
+            try
+            {
+                return NetworkInterface.GetAllNetworkInterfaces()
+                    .Where(i => i.OperationalStatus == OperationalStatus.Up)
+                    .SelectMany(i => i.GetIPProperties().UnicastAddresses)
+                    .Select(a => a.Address)
+                    .Where(ip => ip.AddressFamily == AddressFamily.InterNetworkV6)
+                    .Where(ip => ip.ScopeId != 0)
+                    .Any();
+            }
+            catch (SocketException)
+            {
+                return false;
+            }
+        }
+    }
+}
